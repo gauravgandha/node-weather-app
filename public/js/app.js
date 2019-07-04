9559643f2989eed7064b280a581e64ea83f7aee9
Gandha <GGandha@luxoft.com>
2019-07-04 16:16:09 +0530

    Changed for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,5 @@
 console.log('Client side javascript file is loaded!')
 
-// fetch('http://localhost:3000/weather?address=!##').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-
-//         }
-//         else {
-//             console.log(data.forecast)
-//             console.log(data.location)
-//         }
-//     })
-// })
-
 const queryForm = document.querySelector('form')
 
 const search = document.querySelector('input')
@@ -26,7 +13,7 @@ queryForm.addEventListener('submit', (e) =>{
     const location = search.value
 
 
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             console.log(data.error)
